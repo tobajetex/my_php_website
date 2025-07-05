0b0322f1919ebb2f8f1e31abb273e30781a27bb9
Oloruntoba Jetawo <jetawotobajetex@gmail.com>
2025-07-05 22:11:30 +0100

    Updated to secure config loading

--- config.php
+++ config.php
@@ -1,10 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = ""; // Leave blank for XAMPP
-$db = "bincomtest";
+$config = include 'config.env.php';
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli(
+    $config['servername'],
+    $config['username'],
+    $config['password'],
+    $config['dbname']
+);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
